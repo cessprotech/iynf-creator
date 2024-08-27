28b7488bfe8beb463f2f84a571a3ef851f9dc35f
chucky-golden <chukschibyke92@gmail.com>
2024-08-27 18:51:07 +0100

    jobs

--- src/app/me.controller.ts
+++ src/app/me.controller.ts
@@ -70,25 +70,15 @@ export class MeController {
   async getMyJobs(@Query() query, @Req() req) {
     const { otherQuery, paginateOptions } = QueryOptions(query, true);
 
-    // paginateOptions.populate = [
-    //   { path: 'bidsCount' },
-    //   { path: 'review' },
-    //   { path: 'creator' },
-    //   // { path: 'influencer' },
-    //   { path: 'influencer', options: { populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar' ] }]} },
-    // ];
-
     paginateOptions.populate = [
       { path: 'bidsCount' },
       { path: 'review' },
-      { path: 'creator', select: ['creatorId', 'userId'], populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar', 'country'] }] },
-      { path: 'influencer', select: ['influencerId', 'userId'], populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar', 'country'] }] },
-      
+      { path: 'creator' },
+      // { path: 'influencer' },
+      { path: 'influencer', options: { populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar' ] }]} },
     ];
 
-    return await this.jobService.getAllWithAggregate(otherQuery, paginateOptions);
-
-    // return await this.jobService.getMyJobs(otherQuery, req.user.creatorId, paginateOptions);
+    return await this.jobService.getMyJobs(otherQuery, req.user.creatorId, paginateOptions);
   }
 
   @Get(`${jobsRoute}/:id/single`)
