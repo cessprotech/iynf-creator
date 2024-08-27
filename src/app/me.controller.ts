d1f9becd1d69b92c1f5ba46bc7e6c124be1500b9
chucky-golden <chukschibyke92@gmail.com>
2024-08-27 12:05:10 +0100

    balance

--- src/app/me.controller.ts
+++ src/app/me.controller.ts
@@ -74,8 +74,7 @@ export class MeController {
       { path: 'bidsCount' },
       { path: 'review' },
       { path: 'creator' },
-      // { path: 'influencer' },
-      { path: 'influencer', select: ['influencerId', 'userId'], populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar', 'country'], unwindType: 1 }], unwindType: 1 },
+      { path: 'influencer' },
     ];
 
     return await this.jobService.getMyJobs(otherQuery, req.user.creatorId, paginateOptions);
