c5377d1f6922b957bf6de07d2ed2dd9cb7cb8d91
chucky-golden <chukschibyke92@gmail.com>
2024-08-27 22:46:58 +0100

    jobs

--- src/app/me.controller.ts
+++ src/app/me.controller.ts
@@ -71,7 +71,7 @@ export class MeController {
     const { otherQuery, paginateOptions } = QueryOptions(query, true);
 
     paginateOptions.populate = [
-      { path: 'influencer', select: ['influencerId', 'userId'], populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar'], unwindType: 1 }], unwindType: 1 },
+      // { path: 'influencer', select: ['influencerId', 'userId'], populate: [{ path: 'user', select: ['firstName', 'lastName', 'avatar'], unwindType: 1 }], unwindType: 1 },
       { path: 'bidsCount' },
       { path: 'review' },
       { path: 'creator' },
