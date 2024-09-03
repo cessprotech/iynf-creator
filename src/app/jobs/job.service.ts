e10e899a50ef3ad078e726a412b17d515974e3cf
chucky-golden <chukschibyke92@gmail.com>
2024-09-03 18:45:11 +0100

    email

--- src/app/jobs/job.service.ts
+++ src/app/jobs/job.service.ts
@@ -426,7 +426,7 @@ export class JobService {
   async deleteRequest(id: string) {
     console.log('id deleted', id);
     
-    const bid = await this.connection.db.collection('bids').findByIdAndDelete(id);
+    const bid = await this.connection.db.collection('bids').deleteOne({ _id: id })
 
     return bid
   }
